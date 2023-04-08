eb8ed601e336b8a103f88027caaab8b447e74a70
marielletheguzman <marielledeguzman2020@gmail.com>
2023-04-09 04:21:08 +0800

    Added toast and convert into admin

--- src/app/Admin/login/login.component.ts
+++ src/app/Admin/login/login.component.ts
@@ -2,42 +2,59 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthServiceService } from 'src/app/auth-service.service';
 import { Router } from '@angular/router';
+import { Message } from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  providers: [MessageService],
 })
 export class LoginComponent implements OnInit {
   value: string | undefined;
   formGroup!: FormGroup;
+  messages!: Message[];
+
   constructor(
     private authService: AuthServiceService,
-    private router: Router
+    private router: Router,
+    private messageService: MessageService // Inject MessageService here
   ) {}
+
   ngOnInit() {
     this.initForm();
   }
+
   initForm() {
     this.formGroup = new FormGroup({
       username: new FormControl('', [Validators.required]),
       password: new FormControl('', [Validators.required]),
     });
   }
-
   loginProcess() {
     if (this.formGroup.valid) {
       this.authService.login(this.formGroup.value).subscribe((result) => {
         if (result.success) {
           console.log(result);
-          this.authService.setToken(result.token); // store the token in local storage
+          this.authService.setToken(result.token);
           alert(result.message);
-          this.router.navigate(['/home']);
+          // this.router.navigate(['/home']);
           console.log('home');
         } else {
-          alert(result.message);
-          this.authService.setToken(result.token); // store the token in local storage
-          this.router.navigate(['/home']);
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Logged in Successfully',
+          });
+          timer(1000)
+            .toPromise()
+            .then((done) => {
+              this.authService.setToken(result.token);
+              this.router.navigate(['/home']);
+            });
+          // this.router.navigate(['/home']);
           console.log('success');
         }
       });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,34 +4,45 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './user/login/login.component';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './user/home/home.component';
+
 import { AuthGuard } from './auth.guard';
 import { ButtonModule } from 'primeng/button';
 import { PasswordModule } from 'primeng/password';
 import { InputTextModule } from 'primeng/inputtext';
+import { MessagesModule } from 'primeng/messages';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { HomeComponent } from './Admin/home/home.component';
+import { LoginComponent } from './Admin/login/login.component';
 
-const routes: Routes = [
+const admin: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
 ];
-
+const user: Routes = [
+  { path: '', redirectTo: 'login-user', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+];
 @NgModule({
   declarations: [AppComponent, LoginComponent, HomeComponent],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(admin),
+    RouterModule.forRoot(user),
     ButtonModule,
     BrowserAnimationsModule,
     PasswordModule,
     InputTextModule,
+    MessagesModule,
+    ToastModule,
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent],
   exports: [RouterModule],
 })
